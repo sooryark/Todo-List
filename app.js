65b569465157f063c1127cf0bf31cfdc00c102d1
R.soorya <sooryayaron147@gmail.com>
2023-02-08 15:16:44 +0530

    adding dynamic id in js

--- app.js
+++ app.js
@@ -5,42 +5,45 @@ const addbtn = document.getElementById("addbtn");
 const displayEl = document.getElementById("display-task");
 const imgContainer = document.getElementById("imgEl");
 const totalTask =  document.getElementById("total-task");
+
  stroringArr = [];
+ inputValue.focus();
 
 //add function
 function addtask(){
    const userInput = inputValue.value;//getting user input
-    
+   
    if(userInput == ""){
     return alert("please Enter your Task");// alert users to enter a tasks
    }
-   if(userInput.trim() == ""){
+   if(userInput.trim() == ""){//remove empty space
     return;
    }
 
    //console.log(userInput);
    inputValue.value = "";//empty the input ui
-      
+      taskId = Math.random().toString();//getting random id
 
-   const taskObj = {};
+
+   const taskObj = {};             //obj for storing value,state,id
    taskObj.value = userInput;
    taskObj.isCompleted = false;
-   stroringArr.push(taskObj);
-
-
-
+   taskObj.id = taskId;
+   console.log(taskId)
+   stroringArr.push(taskObj); //pushing taskObj to StoringArr
    
    setTask();
-   createTask(userInput,false);
-   newUI();
-
-   
+   createTask(userInput,false,taskId);
+   newUI();  
 }
 
-
-function createTask(userInput,isCompleted){
-    //createting li element
+//createting li element
+function createTask(userInput,isCompleted,taskId){
+  
     const liElement = document.createElement("li");
+
+    liElement.setAttribute("id",taskId);
+
      if(isCompleted){
         liElement.setAttribute("class","task-items active");
      }else{
@@ -67,16 +70,18 @@ function createTask(userInput,isCompleted){
     liElement.append(completedBtn);
     liElement.append(deletedBtn);
     ulElelement.append(liElement);
-
 }
+
+
+//working on compltebtn
 function completed(){
     this.parentElement.classList.toggle("active");
-    const taskText = this.parentElement.innerText;
-          console.log(taskText)
+    const taskId = this.parentElement.getAttribute("id");
+    console.log (taskId)
     for(let i=0;i<stroringArr.length;i++){
         const taskObj = stroringArr[i];
-        console.log(taskObj)
-        if(taskObj.value == taskText){
+        //console.log(taskObj)
+        if(taskObj.id == taskId){
            taskObj.isCompleted = !taskObj.isCompleted;
             console.log(!taskObj.isState)
         }
@@ -84,10 +89,11 @@ function completed(){
     setTask();
 }
 
+//working on delete btn
 function remove(){
-    const delText = this.parentElement.innerText;
+    const delText = this.parentElement.getAttribute("id");
     for(let i=0;i<stroringArr.length;i++){
-        if(stroringArr[i].value == delText){
+        if(stroringArr[i].id == delText){
            stroringArr.splice(i,1);
         }
     }
@@ -98,11 +104,13 @@ function remove(){
 }
 
 
-
-
+//set tasks in localstorage
 function setTask(){
     const storeEl = localStorage.setItem("tasks",JSON.stringify(stroringArr));
 }
+
+
+//get tasks from local storage
 function getTask(){
    const gettingValue = JSON.parse(localStorage.getItem("tasks"));
    if(!gettingValue){
@@ -110,23 +118,21 @@ function getTask(){
    }
    //console.log(gettingValue);
    for(index in gettingValue){
-       createTask(gettingValue[index].value,gettingValue[index].isCompleted);
+       createTask(gettingValue[index].value,gettingValue[index].isCompleted,gettingValue[index].id);
        stroringArr.push(gettingValue[index]);
    }
-
+ 
 }
 getTask();
 
+//displaying total task Items
 function newUI(){
-        totalTask.innerHTML= `No of Total-task ${ulElelement.children.length}`;
+        totalTask.innerHTML= `No of Total-task : <span>${ulElelement.children.length}</span>`;
     //console.log(displayEl.children);
 }
+newUI()
 
-newUI();
-
-
-
-
+//add task using enter key 
 function handlekey(event){
     if(event.key == "Enter"){
         addtask();
